b2793159e574535c2c8dc2351512f1e92b361d15
Omid <o.ghozatlou@reply.com>
2026-02-09 22:01:47 +0200

    add installed dependency deploy problem

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ import tseslint from 'typescript-eslint';
 import localPlugin from './scripts/eslint_rules/local-plugin.js';
 
 export default defineConfig([
-  globalIgnores(['**/node_modules', '**/build/']),
+  globalIgnores(['**/node_modules', '**/build/', 'website/']),
   importPlugin.flatConfigs.typescript,
   {
     languageOptions: {
